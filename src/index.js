e5851ee0fb64a43b0965ea62cae6895c33498d4f
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-22 15:41:48 +0000

    *

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput= document.querySelector("#search-form-input");
-console.log(searchInput.Value);
+console.log(searchInput.value);
 }
 
-
-let searchFormElement= document.querySelector("#search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
