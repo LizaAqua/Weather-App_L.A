614c6b1acee50092c7e94132cdec5b2341588c27
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-22 15:32:08 +0000

    adding to JavaScript file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Hey! How's it going");
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput= document.querySelector("#search-form-input");
+console.log(searchInput.Value);
+}
+
+
+let searchFormElement= document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
