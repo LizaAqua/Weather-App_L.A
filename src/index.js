c2a1338cff96241f69e940517bf19dbf463ab1f5
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-22 16:41:10 +0000

    added the apiKey to the HTML and apiKey & apiURL to JavaScript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function searchCity(city) {
+  let apiKey="abfec4b8be01abt65a50c3e50o5aa3d7"
+  let apiURL="https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"
+console.log(apiURL);
+
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput= document.querySelector("#search-form-input");
     let cityELement= document.querySelector ("#city");
-    cityELement.innerHTML= searchInput.value;
+cityELement.innerHTML= searchInput.value;
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
