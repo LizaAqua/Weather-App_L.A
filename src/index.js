fee0ac94deb494307f043d1bcdc5dce0b19e4b9e
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-30 15:16:11 +0000

    *

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ function refreshWeather(response) {
     console.log(response.data.condition.description);
     cityELement.innerHTML= response.data.city;
     descriptionElement.innerHTML=response.data.condition.description;
-    humidityElement.innerHTML=${response.data.temperature.humidity}%;
+    humidityElement.innerHTML='${response.data.temperature.humidity}%';
     temperatureElement.innerHTML= Math.round(temperature);
-
-    console.log(response.data.condition.description); cityElement.innerHTML = response.data.city; descriptionElement.innerHTML = response.data.condition.description; humidityElement.innerHTML = ${response.data.temperature.humidity}%; temperatureElement.innerHTML = Math.round(temperature);
 
 
 }
