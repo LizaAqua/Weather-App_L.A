6ae9f206b48941ce59c884bb3f4deaed523af928
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-30 18:47:17 +0000

    *

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function refreshWeather(response) {
     
     cityELement.innerHTML= response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
-     timeElement.innerHTML = formatDate(date);
+    timeElement.innerHTML= "Tuesday 21:48"
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML= Math.round(temperature);
