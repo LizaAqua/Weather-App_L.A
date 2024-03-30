e5948b52af850ad106eb27fd0f0ba1480c7d6b32
LizaAqua <148656169+LizaAqua@users.noreply.github.com>
2024-03-30 16:36:21 +0000

    adding the time and date element

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ function refreshWeather(response) {
     let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
 
 
     console.log(response.data.temperature.humidity);
     
     cityELement.innerHTML= response.data.city;
+    timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
